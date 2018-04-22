2cfb6b4b15d2aeebf74840d2fdc6d590a3be14c0
meiyuk <yenenewong@gmail.com>
2018-04-22 18:09:21 +0800

    discuss_tab +discuss layout

---
+++ app/src/main/java/app/comps456f/Category1.java
@@ -0,0 +1,22 @@
+package app.comps456f;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by meiyuk on 22/4/2018.
+ */
+
+public class Category1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.category1);
+    }
+
+
+}
